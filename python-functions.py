4eac5d7005166e04b9d894c9671f87be8f56131e
dcharr16 <davidcraigharrison@gmail.com>
2022-04-07 09:13:36 -0400

    complete functions lab

---
+++ python-functions.py
@@ -0,0 +1,56 @@
+#Write a function named sum_to that accepts a single integer, n, and returns the sum of the integers from 1 to n.
+
+#for example 
+#sum_to(6)  # returns 21
+#sum_to(10) # returns 55
+
+def sum_to(num):
+  sum = 0
+  for n in range(1, num + 1):
+    sum += n
+  return sum
+print (sum_to (6))
+
+# 2. Write a function named `largest` that takes a list of numbers as an argument and returns the largest number in that list.
+    
+#     For example:
+
+# largest([1, 2, 3, 4, 0])  # returns 4
+# largest([10, 4, 2, 231, 91, 54])  # returns 231
+
+def largest(nums):
+  largest = 0
+  for num in nums:
+    if num > largest:
+      largest = num
+  return largest
+print (largest ([1, 2, 3, 4, 0]))
+
+
+# 3. Write a function named `occurrences` that takes two string arguments as input and counts the number of occurrences of the second string inside the first string.
+    
+#     For example:
+
+# occurrences('fleep floop', 'e')   # returns 2
+# occurrences('fleep floop', 'p')   # returns 2
+# occurrences('fleep floop', 'ee')  # returns 1
+# occurrences('fleep floop', 'fe')  # returns 0
+
+def occurances (string, substr):
+  return string.count(substr)
+print(occurances('fleep floop', 'e'))
+
+# 4. Write a function named `product` that takes an *arbitrary* number of numbers, multiplies them all together, and returns the product. HINT: Review your notes on `args`.
+    
+#     For example:
+
+# product(-1, 4) # returns -4
+# product(2, 5, 5) # returns 50
+# product(4, 0.5, 5) # returns 10.0
+
+def product(*args):
+  product = 1
+  for arg in args:
+    product *= arg
+  return product
+print(product(2, 5, 5))
